38558d9c9fa81679303414f62f300601be065804
cyphenstine <aritrakumar093@gmail.com>
2024-08-27 00:21:10 +0530

    changed launch.sh

--- launch.sh
+++ launch.sh
@@ -10,6 +10,6 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
+#polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
 polybar bar4 2>&1 | tee -a /tmp/polybar3.log & disown
 echo "Bars launched..."
