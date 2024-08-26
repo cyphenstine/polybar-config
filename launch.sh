7bef8416d61656488c356148b7a38c51f182bf5d
cyphenstine <aritrakumar093@gmail.com>
2024-08-27 01:38:12 +0530

    modified config.ini & launch.sh

--- launch.sh
+++ launch.sh
@@ -8,8 +8,13 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2 and bar3
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-#polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
-polybar bar4 2>&1 | tee -a /tmp/polybar3.log & disown
+polybar xworkspaces 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar xwindow 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar date 2>&1 | tee -a /tmp/polybar3.log & disown
+polybar pulseaudio 2>&1 | tee -a /tmp/polybar4.log & disown
+polybar battery 2>&1 | tee -a /tmp/polybar5.log & disown
+polybar cpu 2>&1 | tee -a /tmp/polybar6.log & disown
+polybar memory 2>&1 | tee -a /tmp/polybar7.log & disown
+polybar filesystem 2>&1 | tee -a /tmp/polybar8.log & disown
+polybar network 2>&1 | tee -a /tmp/polybar9.log & disown
 echo "Bars launched..."
