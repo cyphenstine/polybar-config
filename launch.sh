68bcfaef49e3a0861e4ebf6628ef4da33133d0bd
cyphenstine <aritrakumar093@gmail.com>
2024-09-16 00:18:33 +0530

    modified config.ini & launch.sh

--- launch.sh
+++ launch.sh
@@ -6,8 +6,12 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
+# Launch bar1 and bar2 and bar3
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-
+polybar xworkspaces 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar datentime 2>&1 | tee -a /tmp/polybar3.log & disown
+polybar cpu 2>&1 | tee -a /tmp/polybar6.log & disown
+polybar memory 2>&1 | tee -a /tmp/polybar7.log & disown
+polybar battery 2>&1 | tee -a /tmp/polybar5.log & disown
+polybar network 2>&1 | tee -a /tmp/polybar9.log & disown
 echo "Bars launched..."
